8b7f65dd28582984140a010941e05efee79f5a9e
Hualdo <hucastillo@gmail.com>
2021-11-05 18:19:21 -0500

    Programado acordion, mostrar y ocultar detalle, las flechas también cambian al dar click

--- acordion.js
+++ acordion.js
@@ -1,3 +1,32 @@
-function ver(){
-    document.getElementById('d1').style.display='block'
+function ver1(){
+    document.getElementById('d1').classList.toggle('visible')
+    document.getElementById('l1').classList.toggle('negrita')
+    document.getElementById('icon1').classList.toggle('rotate')
     }
+
+function ver2(){
+  document.getElementById('d2').classList.toggle('visible')
+  
+  document.getElementById('l2').classList.toggle('negrita')
+  document.getElementById('icon2').classList.toggle('rotate')
+  }    
+
+function ver3(){
+    document.getElementById('d3').classList.toggle('visible')
+    document.getElementById('l3').classList.toggle('negrita')
+    document.getElementById('icon3').classList.toggle('rotate')
+    }    
+
+function ver4(){
+    document.getElementById('d4').classList.toggle('visible')
+    document.getElementById('l4').classList.toggle('negrita')
+    document.getElementById('icon4').classList.toggle('rotate')
+    }
+
+function ver5(){
+    document.getElementById('d5').classList.toggle('visible')
+    document.getElementById('l5').classList.toggle('negrita')
+    document.getElementById('icon5').classList.toggle('rotate')
+    }   
+    
+  
